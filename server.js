bd9951408f6a67f4549c005b31c2ae67c67c6f7d
Vladas Barysas <14954655+vladasbarisas@users.noreply.github.com>
2020-05-07 12:54:08 +0300

    added server port as config entry

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var PORT = process.env.PORT || 3000;
+var config = require("./config.json");
+var PORT = process.env.PORT || config.serverPort || 3000;
 var express = require("express");
 var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var fs = require("fs");
-var config = require("./config.json");
 
 // HTTP server
 app.use(express.static(__dirname + "/public"));
